874d1bd772ff87c32229ca5c06ff8ae5d422f14c
crystian182 <cristian.vergallo@gmail.com>
2018-12-12 13:36:36 +0100

    query and generator fix

--- src/main/java/it/unisalento/se/saw/repositories/TypeLessonRepository.java
+++ src/main/java/it/unisalento/se/saw/repositories/TypeLessonRepository.java
@@ -13,6 +13,6 @@ import it.unisalento.se.saw.domain.TypeLesson;
 @Repository
 public interface TypeLessonRepository extends JpaRepository<TypeLesson, Integer>{
 
-	@Query("SELECT t FROM TypeLesson t WHERE t.scheduler.idscheduler=:(SELECT s.idscheduler FROM Scheduler s, Term m WHERE s.term.idterm=m.idterm AND s.degreeCourse.iddegreeCourse=:idCourse AND NOW()>=m.start AND NOW()<=m.end;);")
+	@Query("SELECT t FROM TypeLesson t WHERE t.scheduler.idscheduler IN (SELECT s.idscheduler FROM Scheduler s, Term m WHERE s.term.idterm=m.idterm AND s.degreeCourse.iddegreeCourse=:idCourse AND NOW()>=m.start AND NOW()<=m.end)")
 	public List<TypeLesson> getCurrentSchedulerByIDCourse(@Param("idCourse")int idCourse);
 }
